01fe0323b4d865ab9f8f618729f99ee8d50cf507
Igor <semenov-ip@mail.ru>
2014-02-12 07:50:49 +0400

    Add new project elements

---
+++ index.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+
+
+
+
+
+
+
+
+<div id="teaser_1"><a href="http://teaserleader_woman.dev/">Тизерная сеть</a></div>
+<script type="text/javascript">document.write('<scr'+'ipt type="text/jav'+'ascript" src="http://teaserleader_woman.dev/_shared/show/index/1/'+escape(document.referrer) + Math.round(Math.random()*100000)+'/"></scr'+'ipt>');</script>
+
+
+<div id="teaser_2"><a href="http://teaserleader_woman.dev/">Тизерная сеть</a></div>
+<script type="text/javascript">document.write('<scr'+'ipt type="text/jav'+'ascript" src="http://teaserleader_woman.dev/_shared/show/index/2/'+escape(document.referrer) + Math.round(Math.random()*100000)+'/"></scr'+'ipt>');</script>
+
+______________________________________________________________________________________________
+
+
+
+
+
+
+
+<div id="teaser_13"><a href="http://teaserleader.dev/">Тизерная сеть</a></div>
+<script type="text/javascript">document.write('<scr'+'ipt type="text/jav'+'ascript" src="http://teaserleader.dev/show/?block_id=13&r='+escape(document.referrer)+'&'+Math.round(Math.random()*100000)+'"></scr'+'ipt>');</script>
---
+++ teaserleader_bloc_example/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <h1>Переходите по ссылкам, а затем жмите в браузере кнопки back/forward</h1>
+    <a href="mylink.html" title="Заголовок связанный с ссылкой My Link">My Link</a>
+    <a href="otherlink.html" title="Заголовок связанный с ссылкой Other Link">Other Link</a>
+  	<div id="teaser_8"><a href="http://teaserleader.dev/">Тизерная сеть</a></div>
+	<script type="text/javascript">document.write('<scr'+'ipt type="text/jav'+'ascript" src="http://teaserleader.dev/show/?block_id=8&r='+escape(document.referrer)+'&'+Math.round(Math.random()*100000)+'"></scr'+'ipt>');</script>
+  </body>
+</html>
