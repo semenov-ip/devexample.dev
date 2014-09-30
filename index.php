17bb28bbcde8f8329692716fc0de0d1a8c964c02
Igor <semenov-ip@mail.ru>
2014-09-30 12:00:54 +0400

    New stady

---
+++ index.php
@@ -0,0 +1,7 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<div id="rotator_8"><img src="http://grizzleads.dev/images/preolader.gif" /></div><script async type="text/javascript" src="http://grizzleads.dev/_shared/show/index/8"></script>
+
+
+
+<div id="rotator_6"><img src="http://grizzleads.dev/images/preolader.gif" /></div><script async type="text/javascript" src="http://grizzleads.dev/_shared/show/index/6"></script>
